f81c74be1278288bd7a4be70f67f0e7c53e4c9d6
dingdongzhao <ddzyan@163.com>
2022-03-04 17:07:28 +0800

    test(优化单元测试): 🧪

--- test/leetCode/q146LRU缓存.test.js
+++ test/leetCode/q146LRU缓存.test.js
@@ -7,11 +7,9 @@ describe("q146LRU缓存", () => {
     const cache = new LRUCache(2);
     cache.put(1, 1); // 缓存是 {1=1}
     cache.put(2, 2); // 缓存是 {1=1, 2=2}
-    cache.output();
     assert.strictEqual(cache.get(1), 1, "返回结果错误");
 
     cache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
-    cache.output();
     assert.strictEqual(cache.get(2), -1, "返回结果错误");
 
     cache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
