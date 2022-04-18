9053051e9f28ac78cd3e5957ff18eefe999943f5
dingdongzhao <ddzyan@163.com>
2022-04-19 00:02:56 +0800

    fix(修复冒泡排序错误，增加排序单元测试): 🐞

--- test/leetCode/q912排序数组.test.js
+++ test/leetCode/q912排序数组.test.js
@@ -2,8 +2,15 @@ const assert = require("assert");
 const mpSort = require("../../leetCode/q912排序数组/冒泡");
 
 describe("q912排序数组", () => {
-  it("冒泡排序 test", () => {
-    const res = mpSort([1, 3, 4, 2, 6, 9]);
-    assert.strictEqual(res.toString(), "1,2,3,4,6,9", "返回结果错误");
+  describe("冒泡排序", () => {
+    it("[1, 3, 4, 2, 6, 9] test", () => {
+      const res = mpSort([1, 3, 4, 2, 6, 9]);
+      assert.strictEqual(res.toString(), "1,2,3,4,6,9", "返回结果错误");
+    });
+
+    it("[3, 2, 1] test", () => {
+      const res = mpSort([3, 2, 1]);
+      assert.strictEqual(res.toString(), "1,2,3", "返回结果错误");
+    });
   });
 });
