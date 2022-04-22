72365e1352d01458adffe7782fa8f340d4778434
dingdongzhao <ddzyan@163.com>
2022-04-22 10:42:15 +0800

    refactor(优化冒泡排序): 🦄

--- leetCode/q912排序数组/冒泡/index.js
+++ leetCode/q912排序数组/冒泡/index.js
@@ -1,19 +1,18 @@
 function sort(arr) {
-  if (!arr || !Array.isArray(arr) || arr.length === 0) {
-    return [];
-  }
+  if (arr.length === 0) return arr;
 
   const len = arr.length;
   for (let i = 0; i < len; i++) {
     let isSort = false;
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
+        isSort = true;
         const tmp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
-        isSort = true;
       }
     }
+
     if (!isSort) {
       break;
     }
--- leetCode/q912排序数组/冒泡/练习版本.js
+++ leetCode/q912排序数组/冒泡/练习版本.js
@@ -1,15 +1,18 @@
 function sort(arr) {
-  if (arr.length === 0) return arr;
+  if (!arr || !Array.isArray(arr) || arr.length === 0) {
+    return [];
+  }
 
   const len = arr.length;
+
   for (let i = 0; i < len; i++) {
     let isSort = false;
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        isSort = true;
         const tmp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
+        isSort = true;
       }
     }
 
